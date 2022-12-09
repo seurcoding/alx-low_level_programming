29a3f119b4c75015470a19b3078a72a517bb2317
seurcoding <mbiseg108@gmail.com>
2022-12-09 06:04:00 -0800

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,17 +6,14 @@
  *
  * Return: Always 0 (success)
  */
-int main(void)
+int main(void) 
+
 {
-	int a;
-
-	long int b;
-
-	long long int c;
-
-	char d;
-
-	float f;
+	int (a);
+	long int (b);
+	long long int (c);
+	char(d);
+	float (f);
 
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
 
