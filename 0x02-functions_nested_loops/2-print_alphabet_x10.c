58b9c4d91f3c1b6fb350377f0ea0eea17cfd6f87
seurcoding <mbiseg108@gmail.com>
2022-12-13 18:55:34 -0800

    print alphabet ten times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,12 +1,10 @@
 #include "main.h"
 /**
  * void print_alphabet_x10
- *
- * Return: Always 0.
+ * Description: function uses_putchar function to print
+ * alphabet in lowercase 10 times
+ * Return: Nothing.
  */
-int main(void)
-
-{
 	void print_alphabet_x10(void)
 {
 	char i;
