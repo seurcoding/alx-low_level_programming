ffa30ae9fb367207646b6a7d0a6c73b346a53d9b
seurcoding <mbiseg108@gmail.com>
2022-12-14 14:57:09 -0800

    10-add

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,29 +1,7 @@
 #include "main.h"
 
-void
-times_table(void)
+int
+add(int a, int b)
 {
-	int c, d, e;
-
-	for (c = 0; c < 10; c++)
-	{
-		for (d = 0; d < 10; d++)
-		{
-			e = d * c;
-
-			if (e / 10 != 0)
-			{
-				_putchar(e / 10 + '0');
-			}
-
-			_putchar(e / 10 + '0');
-
-			if (d != 9)
-			{
-				_putchar(',');
-				_putchar('\t');
-			}
-		}
-		_putchar('\n');
-	}
+	return(a+b);
 }
