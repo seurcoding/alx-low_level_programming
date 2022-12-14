3fe365741f63c4b94d6d09a9d754906d6773fb10
seurcoding <mbiseg108@gmail.com>
2022-12-14 01:40:01 -0800

    5-sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,21 +3,25 @@
 /**
  * print_sign - prints the sign of a num
  * @n:the num to be checked
- * Return: 1 and prints + if n is greater than zero
- * 0 and prints 0 if n is zero
- * -1 and prints - if n is less than zero
+ * Return: 1 and prints '+' if n > 0,0 prints '0' if n = 0,
+ * -1 prints '-'if n < 0
  */
 
 int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar('+');
+		_putchar ('+');
 		return (1);
 	}
 	else if (n == 0)
 	{
-		_putchar('0');
+		_putchar ('0');
 		return (0);
+	}
+	else
+	{
+		_putchar ('-');
+		return (-1);
 	}
 }
