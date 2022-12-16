0aaa8dfea707e1d9f388a2c54e57881b7247bcb8
seurcoding <mbiseg108@gmail.com>
2022-12-15 16:00:48 -0800

    task 0 to task number 10

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mult-multiply 2 int 
+ * @a: first num
+ * @b: second num
+ * Return: result of addition of a and b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
