5c95c7f38d4134dd64d585f50dbe571673961639
seurcoding <mbiseg108@gmail.com>
2022-12-19 06:08:18 -0800

    Battery st

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * reset_to_98 - update pointer value to 98
+ * @n: parameter to be provided
+ * Return: Always 0 (Success)
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This is the project c-pointer,arrays and strings
